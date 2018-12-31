67ce29e36ce3e0560137c3c5e0a11a7972f7b706
asduj <asduj@ya.ru>
2018-12-31 18:31:17 +0300

    - Simplify tests.

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
-from typing import Tuple
+from typing import Tuple, Any
 
 from polymorphism import Polymorphism
 import pytest  # type: ignore
 
 
-def test_simpler_overload():
+def test_overload():
     # noinspection PyMethodMayBeStatic,PyRedeclaration
     class Simple(Polymorphism):
         def calc(self, x: int, y: int) -> type:
@@ -19,21 +19,7 @@ def test_simpler_overload():
     assert simple.calc('1', '2') == str
 
 
-def test_simple_dispatch():
-    # noinspection PyMethodMayBeStatic,PyRedeclaration
-    class Simple(Polymorphism):
-        def calc(self, x: int, y: int) -> type:
-            return int
-
-        def calc(self, x: str, y: str) -> type:
-            return self.calc(int(x), int(y))
-
-    simple = Simple()
-
-    assert simple.calc('1', '2') == int
-
-
-def test_choice_dispatch():
+def test_internal_dispatch():
     # noinspection PyMethodMayBeStatic,PyRedeclaration
     class Simple(Polymorphism):
         def calc(self, x: int, y: int) -> type:
@@ -42,17 +28,29 @@ def test_choice_dispatch():
         def calc(self, x: float, y: float) -> type:
             return float
 
-        def calc(self, x: str, y: str, *, cast: type = float) -> type:
+        def calc(self, x: str, y: str, *, cast: type) -> type:
             return self.calc(cast(x), cast(y))
 
     simple = Simple()
 
-    assert simple.calc(1, 2) == int
     assert simple.calc('1', '2', cast=float) == float
-    assert simple.calc('1', '2', cast=int) == int
 
 
-def test_only_positional():
+def test_dispatch_only_keyword():
+    # noinspection PyMethodMayBeStatic,PyRedeclaration
+    class Simple(Polymorphism):
+        def calc(self, x: str, y: str, *, cast: type) -> Any:
+            raise Exception
+
+        def calc(self, x: str, y: str) -> type:
+            return str
+
+    simple = Simple()
+
+    assert simple.calc('1', '2') == str
+
+
+def test_call_only_positional():
     # noinspection PyMethodMayBeStatic,PyRedeclaration
     class Simple(Polymorphism):
         def calc(self, x: int, y: int) -> type:
